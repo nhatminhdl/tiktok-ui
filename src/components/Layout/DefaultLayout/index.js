ea37cfeda39fbef136b7ae58cffd06318b6726dc
nhatminhdl <thinhatminhdl@gmail.com>
2023-06-16 22:49:54 +0700

    16/03

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,20 @@
+import Header from "~/components/Layout/Components/Header";
+import styles from "./Defaulayout.module.scss";
+import classNames from "classnames/bind";
+import Sidebar from "./Sidebar";
+
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx("wrapper")}>
+            <Header />
+            <div className={cx("container")}>
+                <Sidebar />
+                <div className={cx("content")}>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
